5889e4c1867ea0585f2f42a9da3072dccfc710e5
Kristin Hill <krstinhill4211@gmail.com>
2022-03-21 13:25:35 -0700

    tiled map loading

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ class MyGame extends Phaser.Scene
 
     preload ()
     {
-this.load.image('base_tiles', './src/assets/tilemaps/bug.png')      
-this.load.tilemapTiledJSON('map', './src/assets/tilemaps/test_tiled.json');
-this.load.image('tiles1', './src/assets/tilesets/Tile001.png');
-this.load.image('tiles2', './src/assets/tilesets/Tile002.png');
+       // this.load.image('base_tiles', './src/assets/tilemaps/bug.png')      
+        this.load.tilemapTiledJSON('map', './src/assets/tilemaps/GHLobby1.json');
+        this.load.image('lobbyTiles', './src/assets/tilesets/LobbyTiles.png');
+        this.load.image('text', './src/assets/tilesets/Text.png');
   
     }
-      
+
     create ()
     {
         console.log(this.cache.tilemap.get('map').data);
@@ -24,10 +24,13 @@ this.load.image('tiles2', './src/assets/tilesets/Tile002.png');
 
         const map = this.make.tilemap({key: 'map', tileWidth: 32, tileHeight: 32});
        
-        const tileset = map.addTilesetImage('Tile 1', 'tiles1');
-        const tileset2 = map.addTilesetImage('tile 2', 'tiles2');
+        const tileset = map.addTilesetImage('Walls and Floor', 'lobbyTiles');
+        const tileset2 = map.addTilesetImage('Text', 'text');
+        map.createLayer('Floor and Wall', tileset);
         map.createLayer('Furniture', tileset);
-        map.createLayer('Tile Layer 2', tileset2);
+        map.createLayer('Objects', tileset);
+        map.createLayer('Letters', tileset2);
+        map.createLayer('Curtains', tileset);
     }
 }
 
