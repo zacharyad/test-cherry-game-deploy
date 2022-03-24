0d081b5f60c0ffc820d1d32a1f1a8033dfd1a816
mschwartz8 <42093190+mschwartz8@users.noreply.github.com>
2022-03-24 17:00:25 -0400

    rendered furniture and spacestation to map

--- src/scenes/Engineering.js
+++ src/scenes/Engineering.js
@@ -8,6 +8,9 @@ export default class Engineering extends Phaser.Scene {
     preload(){
         this.load.tilemapTiledJSON('map', '../public/assets/tilemaps/engineering.json');
         this.load.image('engineeringFloor', '../public/assets/tilesets/LobbyTiles.png');
+        this.load.image('spaceStationpng', '../public/assets/tilesets/neotiles.png');
+        this.load.image('furniturepng', '../public/assets/tilesets/shop-and-hospital.png')
+
     }
     
     create () {
@@ -20,9 +23,19 @@ export default class Engineering extends Phaser.Scene {
             tileWidth: 32,
             tileHeight: 32,
           });
-
+        
+          // add tileset image to tilemap p1= name of tileset in Tiled, p2= is key in png in preload
         const engineeringTiles = map.addTilesetImage('LobbyTiles', 'engineeringFloor');
+        const spaceStationTiles = map.addTilesetImage('spacestation', 'spaceStationpng');
+        const furnitureTiles = map.addTilesetImage('shop-and-hospital', 'furniturepng');
+
+        // create layer in order, p1=name of layer in Tiled, p2= tileset image constant it's referring to
         let floorLayer = map.createLayer('Floor', engineeringTiles);
+        let wallLayer = map.createLayer('Wall', engineeringTiles);
+        let spaceStation = map.createLayer('spaceStation', spaceStationTiles);
+        let furnitureLayer = map.createLayer('Furniture', furnitureTiles);
+
+        
 
     }
 
