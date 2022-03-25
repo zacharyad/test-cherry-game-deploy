556a75b50a19d754850d6ac599e9bfea1fc46b43
LPM <11pm@LPMs-MacBook-Air.local>
2022-03-25 16:49:06 -0400

    base tech room rendering

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import Phaser from 'phaser';
-import config from './config/config';
-import Lobby from './scenes/Lobby'
-import Engineering from './scenes/Engineering'
+import Phaser from "phaser";
+import config from "./config/config";
+import Lobby from "./scenes/Lobby";
+import Engineering from "./scenes/Engineering";
+import Technology from "./scenes/Technology";
 let cursors;
 let grace;
 
@@ -10,21 +11,17 @@ class Game extends Phaser.Game {
     super(config);
     // include any scenes in the game
     this.scene.add("Lobby", Lobby);
-    this.scene.add("Engineering", Engineering)
-    this.scene.start("Engineering")
+    this.scene.add("Engineering", Engineering);
+    this.scene.add("Technology", Technology);
+    this.scene.start("Engineering");
     // change scene.start to see if it works to test it & make sure you import it on top
 
     // this.scene.physics.world.enable(this);
     // this.scene.add.existing(this);
-    
   }
-
-  
 }
 
 // 29 map.createLayer('Layer name in Tiled', variable name)
 // 27 variable name = map.addTilesetImage('Tileset name in Tiled', tileset .png file)
-
-
 
 const game = new Game(config);
