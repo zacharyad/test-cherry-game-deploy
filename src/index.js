4ee9f7ac59f6ccb660b2f57c8c3b7a9fdecba48e
mschwartz8 <42093190+mschwartz8@users.noreply.github.com>
2022-03-25 11:42:19 -0400

    changed index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class Game extends Phaser.Game {
     // include any scenes in the game
     this.scene.add("Lobby", Lobby);
     this.scene.add("Engineering", Engineering)
-    this.scene.start("Engineering")
+    this.scene.start("Lobby")
     // change scene.start to see if it works to test it & make sure you import it on top
 
     // this.scene.physics.world.enable(this);
