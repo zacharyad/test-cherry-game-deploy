0ce10325e607099f5f27ab5472455cb88427c74a
LPM <11pm@LPMs-MacBook-Air.local>
2022-03-28 19:42:51 -0400

    embedded new tileset

--- public/assets/tilemaps/Computer01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Computer01" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="../../../../Downloads/Pico8Computer by PXLerGame/Computer01.png" width="64" height="64"/>
-</tileset>
--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ class Game extends Phaser.Game {
     this.scene.add("Technology", Technology);
     this.scene.add("Science", Science);
     this.scene.add("Math", Math);
-    this.scene.start("Lobby");
+    this.scene.start("Technology");
 
     // change scene.start to see if it works to test it & make sure you import it on top
 
