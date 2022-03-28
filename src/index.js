77e779ad6ac2575b935558a5f101d2f782b41f2d
Lauren Noble <lauren.noble3@gmail.com>
2022-03-28 14:31:54 -0400

    changed start scene back to lobby

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ class Game extends Phaser.Game {
     this.scene.add("Engineering", Engineering);
     this.scene.add("Technology", Technology);
     this.scene.add("Science", Science);
-    this.scene.start("Science");
+    this.scene.start("Lobby");
 
     // change scene.start to see if it works to test it & make sure you import it on top
 
