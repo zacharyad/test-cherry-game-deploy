7a3a72c3a6bc12ea2ba02ffab7a7931021c575ad
mschwartz8 <42093190+mschwartz8@users.noreply.github.com>
2022-03-28 21:25:23 -0400

    centered text of game in okay but not perfect way

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ class Game extends Phaser.Game {
     this.scene.add("Science", Science);
     this.scene.add("Math", Math);
     this.scene.add("Scrammble", Scrammble)
-    this.scene.start("Engineering");
+    this.scene.start("Scrammble");
 
     // change scene.start to see if it works to test it & make sure you import it on top
 
